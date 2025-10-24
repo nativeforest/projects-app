8ad8f52186f36111a2b8da714525c8614807fe4d
nativeforest <andressdark@gmail.com>
2025-10-24 08:54:24 -0500

    feat: global singleton prisma

---
+++ src/app/lib/prismaConfig/prisma.ts
@@ -0,0 +1,16 @@
+import { PrismaClient } from "@/generated/prisma/client";
+
+// import with out use @
+
+// Prevent multiple instances of Prisma Client in development
+declare global {
+  var prisma: PrismaClient | undefined;
+}
+
+export const prisma = global.prisma || new PrismaClient();
+
+if (process.env.NODE_ENV !== 'production') {
+  global.prisma = prisma;
+}
+
+export default prisma;
